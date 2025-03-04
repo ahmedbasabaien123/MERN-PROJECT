cde0bd9c7eebb380863b2daed977a1d08e0dcf52
ahmed <ahmedmb703@gmail.com>
2025-03-04 10:59:21 +0100

    crud getallusers + updateuser + creer un user + delete user + follow et un follow user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,12 @@ const authController = require("../controllers/auth.controller");
 const userController = require('../controllers/user.controller')
 
 router.post("/register", authController.signUp);
-router.get("/", userController.getAllUsers)
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+router.patch("/follow/:id", userController.follow);
+router.patch("/unfollow/:id", userController.unfollow);
+
 
 module.exports = router;
