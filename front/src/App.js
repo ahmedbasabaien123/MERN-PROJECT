aa6e8d313161a44f08690dc456c98d6dd8be5d4a
ahmed <ahmedmb703@gmail.com>
2025-06-15 20:53:40 +0200

    faire un get de jwt quand on est dans lappli et recuperer le id du user

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { UidContext } from "./components/AppContext";
+import axios from "axios";
 import Routes from "./components/routes";
 
-function App() {
-  return <Routes />;
-}
+function App() { 
+  const [uid, setUid] = useState(null);
+  useEffect(() => {
+    const fetchToken = async () => {
+      await axios ({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true, 
+            })
+            .then((res) => { setUid(res.data);
+    
+    })
+    .catch((err) => console.log("No toekn"));
+  };
+  fetchToken();
+  }, [uid]);
+
+  return (
+    <UidContext.Provider value={uid}>
+    <Routes />;
+    </UidContext.Provider>
+  );
+};
 
 export default App;
